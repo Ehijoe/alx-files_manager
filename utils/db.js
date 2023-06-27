188103093fd96308716e698ead3aaf610d9bffba
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-27 20:17:37 +0100

    Fix database utils

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,10 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-  contructor() {
-    console.log('Creating MongoDB client');
-    const host = process.getenv('DB_HOST', 'localhost');
-    const port = process.getenv('DB_PORT', '27017');
-    const database = process.getenv('DB_DATABASE', 'files_manager');
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
     try {
       this.client.connect();
@@ -17,18 +16,21 @@ class DBClient {
 
   isAlive() {
     if (!this.client) {
+      console.log('Client is null');
       return false;
     }
     return this.client.isConnected();
   }
 
   async nbUsers() {
+    if (!this.isAlive()) return 0;
     const users = this.database.collection('users');
     const nb = await users.countDocuments({}, { hint: '_id_' });
     return nb;
   }
 
   async nbFiles() {
+    if (!this.isAlive()) return 0;
     const files = this.database.collection('files');
     const nb = await files.countDocuments({}, { hint: '_id_' });
     return nb;
