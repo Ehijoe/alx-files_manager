f56d46078bfe4029ee71ee8fb5b839c6d80ee36a
lipnganga <philipmurage98@gmail.com>
2023-06-27 19:38:25 +0300

    solution to task 1

--- utilis/db.js
+++ utilis/db.js
@@ -1,78 +1,72 @@
 #!/usr/bin/node
 
 const { MongoClient } = require('mongodb');
-const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-const { pwHashed } = require('./utils');
+const mongo = require('mongodb');
+const { pwdHashed } = require('./utils');
 
 class DBClient {
-    constructor() {
-        const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-        MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-        if (!err) {
-            this.db = client.db(DB_DATABASE);
-        } else {
-            this.db = false;
-        }
-        });
-    }
-    
-    isAlive() {
-        return !!this.db;
-    }
+  constructor() {
+    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
+    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
+    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
+    const dbUrl = `mongodb://${host}:${port}`;
+    this.connected = false;
+    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.connected = true;
+    }).catch((err) => console.log(err.message));
+  }
 
-    async nbUsers() {
-        return this.db.collection('users').countDocuments();
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
+  }
+
+  async createUser(email, password) {
+    const hashedPwd = pwdHashed(password);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').insertOne({ email, password: hashedPwd });
+    return user;
+  }
+
+  async getUser(email) {
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ email }).toArray();
+    if (!user.length) {
+      return null;
     }
+    return user[0];
+  }
 
-    async nbFiles() {
-        return this.db.collection('files').countDocuments();
+  async getUserById(id) {
+    const _id = new mongo.ObjectID(id);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ _id }).toArray();
+    if (!user.length) {
+      return null;
     }
+    return user[0];
+  }
 
-    async findUser(user) {
-        return this.db.collection('users').findOne(user);
+  async userExist(email) {
+    const user = await this.getUser(email);
+    if (user) {
+      return true;
     }
-
-    async createUser(email, password) {
-        return this.db.collection('users').insertOne({ email, password: pwHashed(password) });
-    }
-
-    async updateUser(email, password) {
-        return this.db.collection('users').updateOne({ email }, { $set: { password: pwHashed(password) } });
-    }
-
-    async deleteUser(email) {
-        return this.db.collection('users').deleteOne({ email });
-    }
-
-    async getConnect(email, password) {
-        return this.db.collection('users').findOne({ email, password: pwHashed(password) });
-    }
-
-    async disconnect(token) {
-        return this.db.collection('users').updateOne({ token }, { $set: { token: null } });
-    }
-
-    async saveFile(user, name, type, data) {
-        return this.db.collection('files').insertOne({ userId: user, name, type, data });
-    }
-
-    async getFile(fileId) {
-        return this.db.collection('files').findOne({ _id: fileId });
-    }
-
-    async deleteFile(fileId) {
-
-    }
-
-    async saveImage(user, body) {
-        return this.db.collection('files').insertOne({ userId: user, body });
-    }
-
-    async getImage(imageId) {
-
-    }
+    return false;
+  }
 }
 
 const dbClient = new DBClient();
 module.exports = dbClient;
-
