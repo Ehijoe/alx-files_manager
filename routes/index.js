7145293c74afec77f7890931c1f3819148382763
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-28 12:39:33 +0100

    Add route for creating users

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 function addRoutes(app) {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+
+  app.post('/users', UsersController.postNew);
 }
 
 export default addRoutes;
--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express();
 
 const port = process.env.PORT || '5000';
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 addRoutes(app);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
 
 export default app;
